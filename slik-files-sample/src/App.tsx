5e3f139c6033affb918c256d9bd485dc2000b45a
Arpit Agarwal <arpitagarwal2901@gmail.com>
2022-04-19 11:13:19 -0700

    update upload system

--- slik-files-sample/src/App.tsx
+++ slik-files-sample/src/App.tsx
@@ -1,21 +1,19 @@
 import { useState } from 'react';
 import './App.css';
 import { SlikFiles } from '@sliksafe/slik-files';
-import { Button, Col, Checkbox, Empty, message, Row, Typography, Upload } from 'antd';
+import { Button, Col, Checkbox, Empty, message, Row, Typography, Upload, Input, Alert, Steps } from 'antd';
 
 const { Dragger } = Upload;
 const { Text } = Typography;
+const { Step } = Steps;
 
 function App() {
 
   const [isUploading, setIsUploading] = useState(false)
+  const [apiKey, setAPIKey] = useState<string>()
 
   const [selectedFiles, setSelectedFiles] = useState<any>([])
   const [selectedNetworks, setSelectedNetworks] = useState<string[]>(["filecoin"]);
-
-  const initParams: any = {
-    apiKey: 'aRc7TO9fkyuJZJr51H1gAVLt0z1wrMdXNlIoP5QJS3c='
-  };
 
   function onChange(selectedValues: any) {
     setSelectedNetworks(selectedValues);
@@ -41,10 +39,34 @@ function App() {
   };
 
   async function uploadFile() {
+    if (!!!apiKey) {
+      message.error({
+        key: 'api-key-error',
+        content: 'Please enter an API key'
+      })
+      return
+    }
+
     if (!!!selectedNetworks || selectedNetworks.length === 0) {
-      message.error("Please select a network");
+      message.error({
+        key: 'network-selection-error',
+        content: "Please select at least one network"
+      });
       return;
     }
+
+    if (!!!selectedFiles || selectedFiles.length === 0) {
+      message.error({
+        key: 'file-selection-error',
+        content: 'Please select a file'
+      })
+      return
+    }
+
+    const initParams: any = {
+      apiKey: apiKey
+    };
+
 
     setIsUploading(true)
 
@@ -61,7 +83,10 @@ function App() {
       filesHandler.uploadFile(uploadOptions, (fileId: string, err: any) => {
         console.log("The unique identifier of the file uploaded: ", fileId);
 
-        message.success('File upload finished')
+        message.success({
+          key: 'upload-success',
+          content: 'File upload finished'
+        })
 
         pendingUploadCount -= 1
 
@@ -77,15 +102,15 @@ function App() {
     return (
       <Dragger
         {...props} fileList={selectedFiles}
-        style={{ backgroundColor: 'transparent', width: "70%", margin: "auto" }}>
+        style={{ backgroundColor: 'transparent', width: "50%", borderRadius: '8px', margin: 16 }}>
         <Empty
           description={
-            <Row align='middle' justify='center'>
+            <Row align='middle' justify='start'>
               <Col>
                 <div
                   className='tip-status-card'
                 >
-                  <Text>Drag and Drop a file</Text>
+                  <Text style={{ textAlign: 'center' }}>Drag and Drop a file</Text>
                 </div>
               </Col>
             </Row>
@@ -95,19 +120,66 @@ function App() {
     )
   }
 
+  const renderAPIKeyRegistration = () => {
+    return (
+      <div>
+        <div>
+          <Alert
+            // message="Register API Key"
+            description={`Get an API Key from https://console.developers.sliksafe.com`}
+            type="warning"
+            style={{ borderRadius: '8px', margin: 16, width: '50%' }}
+          />
+        </div>
+        <div>
+          <Input 
+            placeholder="Enter API Key" 
+            style={{ borderRadius: '8px', margin: 16, height: 44, width: '50%' }}
+            onChange={(event) => setAPIKey(event.target.value)} />
+        </div>
+      </div>
+    )
+  }
+
+  const renderStorageNetworkSelection = () => {
+    return (
+      <div>
+        <p style={{ margin: 16 }}>Select as many storage networks<br /></p>
+        <Checkbox.Group style={{ margin: 16 }} options={networkOptions} defaultValue={['filecoin']} onChange={onChange} />
+      </div>
+    )
+  }
+
+  const renderFileSelection = () => {
+    return (
+      <div>
+        {renderDraggerDiv()}
+
+        <Button
+          type="primary"
+          loading={isUploading}
+          style={{ margin: 16 }}
+          onClick={() => uploadFile()}>
+          Upload
+        </Button>
+      </div>
+    )
+  }
+
   return (
-    <div className="App" style={{ textAlign: 'center' }}>
-      <p style={{ margin: 16 }}>Slik Files Demo App<br /></p>
-      <p style={{ margin: 16 }}> Choose Network<br /></p>
-      <Checkbox.Group style={{ margin: 16 }} options={networkOptions} defaultValue={['filecoin']} onChange={onChange} />
-      {renderDraggerDiv()}
-      <Button
-        type="primary"
-        loading={isUploading}
-        style={{ margin: 16 }}
-        onClick={() => uploadFile()}>
-        Upload
-      </Button>
+    <div className="App">
+      <Row justify='center'>
+        <Col span={24} style={{ maxWidth: '60%' }}>
+          <p style={{ margin: 16 }}>Slik Files Demo App<br /></p>
+          
+          <Steps direction="vertical" current={2} progressDot>
+            <Step title="Register API Key" description={renderAPIKeyRegistration()} />
+            <Step title="Select storage networks" description={renderStorageNetworkSelection()} />
+            <Step title="Upload file" description={renderFileSelection()} />
+          </Steps>
+          
+        </Col>
+      </Row>
     </div>
   );
 }
