53211ba15accc78e425704fc373f5b4afe591288
Arpit Agarwal <arpitagarwal2901@gmail.com>
2022-05-18 01:15:44 -0700

    sample nextjs app

--- slik-files-nextjs-sample/pages/index.tsx
+++ slik-files-nextjs-sample/pages/index.tsx
@@ -2,8 +2,106 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import React from 'react'
+import { SlikFiles } from '@sliksafe/slik-files'
+import { useEffect, useState } from 'react'
+import { Upload, message, Divider, Button, Progress } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+import { saveAs } from 'file-saver';
+
+const { Dragger } = Upload;
+
 
 const Home: NextPage = () => {
+
+  const [uploadProgress, setUploadProgress] = useState<number>(0)
+  const [downloadProgress, setDownloadProgress] = useState<number>(0)
+  const [uploadedFileId, setUploadedFileId] = useState<string>()
+
+  const initParams = { apiKey: "VNtQYcfVcNXo4cvNCwEtx" }
+
+  const uploadFile = async (selectedFile: File) => {
+
+    const filesHandler = await SlikFiles.initialize(initParams)
+    let file: File = selectedFile
+
+    // Upload a file with upload options
+    const uploadOptions = {
+      networks: ["filecoin"],
+      walletAddress: "0x5c14E7A5e9D4568Bb8B1ebEE2ceB2E32Faee1311",
+      file: file,
+      isEncrypted: false,
+    }
+
+    filesHandler.uploadFile(uploadOptions, (uploadHandle, fileId, err) => {
+      console.log("The unique identifier of the file uploaded: ", fileId);
+
+      if (!!err) {
+        console.error('Failed to upload file: ', err)
+      } else {
+        console.log("Uploading file progress: ", uploadHandle);
+        setUploadProgress(uploadHandle.percentage);
+
+        if (uploadHandle.status === "uploaded") {
+          console.log("File upload finished");
+          console.log("The unique identifier of the file uploaded: ", fileId);
+
+          setUploadedFileId(fileId)
+        }
+      }
+    });
+
+  }
+
+  const downloadProgressCallback = (value) => {
+    setDownloadProgress(value)
+  }
+
+  const downloadFile = async (fileId: string, callback: any) => {
+    const filesHandler = await SlikFiles.initialize(initParams)
+
+    // Download a file with download options
+    const downloadOptions = {
+      fileId: fileId,
+      walletAddress: "0x5c14E7A5e9D4568Bb8B1ebEE2ceB2E32Faee1311",
+    }
+
+    filesHandler.downloadFile(downloadOptions, (downloadHandle, file, err) => {
+      console.log("Downloading file progress: ", downloadHandle.percentage);
+
+
+      callback(downloadHandle.progress)
+
+      if (downloadHandle.status === "downloaded") {
+        console.log("File download finished");
+        console.log("Downloaded file: ", file);
+
+        /// Save the downloaded file.
+        saveAs(file)
+      }
+    });
+  }
+
+  const props = {
+    name: 'file',
+    multiple: false,
+    onDrop(e) {
+      console.log('Dropped files', e.dataTransfer.files);
+    },
+  };
+  
+  const handleUploadAction = (actionProps: any) => {
+    console.log(`Action props: `, actionProps)
+    uploadFile(actionProps)
+    return undefined
+  }
+
+  const beforeUpload = (file) => {
+    
+    return true
+  }
+  
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,55 +112,47 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://sliksafe.com">Slik!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          Get started by uploading a file to the {' '}
+          <code className={styles.code}>arweave | filecoin | ...</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <Dragger 
+          action={(file: any) => handleUploadAction(file)} 
+          {...props} 
+          style={{ width: '500px', borderRadius: '8px' }} 
+          beforeUpload={beforeUpload}
+          showUploadList={false}
+        >
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p className="ant-upload-text">Click or drag file to this area to upload</p>
+          <p className="ant-upload-hint">
+            Backup to Decentralized Storage
+          </p>
+        </Dragger>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <Progress percent={uploadProgress} style={{ width: '400px', padding: '16px 0px', visibility: uploadProgress <= 10 ? 'hidden' : 'visible'}} />
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        <Divider style={{ width: '300px' }} />
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Button loading={downloadProgress > 10} onClick={() => downloadFile(uploadedFileId, downloadProgressCallback)} disabled={!!uploadedFileId ? false : true}>
+          Download File
+        </Button>
+
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://sliksafe.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Powered by Slik!
         </a>
       </footer>
     </div>
